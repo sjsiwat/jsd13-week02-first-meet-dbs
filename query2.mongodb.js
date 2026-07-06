c196ce25a9d1305149e5d1eefbb28796b4325034
sjsiwat <sj.siwat@gmail.com>
2026-07-06 22:07:53 +0700

    learn and try to understand basic query command of mongoDB

--- query2.mongodb.js
+++ query2.mongodb.js
@@ -73,7 +73,88 @@ db.theaters.find({"location.address.city": "La Quinta"}).count();
 /* ข้อที่1
 How many movies does mentioned American in its plot?
 */
-
 use("sample_mflix");
 db.movies.find({ plot: { $regex: "American", $options: "i" } }).count()
+
+/* ข้อที่2How many movies does end plot (sentence) with the street.
+*/
+use("sample_mflix");
+db.movies.find({ plot: { $regex: "street$", $options: "i" } }).count()
+
+/* ข้อที่3 
+What is data of above examples like?
+*/
+use("sample_mflix");
+db.movies.find({ plot: { $regex: "street$", $options: "i" } })
+
+/* Excercise5 */
+/* ข้อที่ 1
+What are top 5 runtime movies? 
+*/
+use("sample_mflix");
+db.movies.find({}).sort({ runtime: -1 }).limit(5)
+
+/* ข้อที่ 2
+What are top 5 runtime movies that less than 60 minutes?
+*/
+use("sample_mflix");
+db.movies.find({ runtime: { $lt: 60 } }).sort({ runtime: -1}).limit(5)
+
+/* ข้อที่ 3
+I was born in 1955, which 3 movies should I watch in next 10 years?
+*/
+
+use("sample_mflix");
+db.movies.find({ year: { $gt: 1954, $lt: 1966}}).sort({ year: 1}).limit(3)
+
+/* ข้อที่ 4 
+How many movies were released during 1990 - 2000?
+*/
+use("sample_mflix");
+db.movies.find({ released: { $gt: ISODate("1990-01-01T00:00:00Z"), $lt: ISODate("2001-01-01T00:00:00Z")}}).count()
+
+
+
+/* Excercise 6 */
+/*Find all movies in the "movies" collection released between 1950 and 1970 (inclusive)
+ that are documented as being shown in countries located in the USA. */
+
+ db.movies.find({
+  countries: {$in: ["USA"]},
+  year: {$gte: 1950, $lte: 1970}
+})
+
+
+/*What is the number of movies in the "movies" collection 
+with genres "Drama" and "History" released after the year 1970?  */
+
+db.movies.find({ 
+  genres: { $all: ["Drama", "History"] },
+  released: { $gt: ISODate("1970-01-01T00:00:00Z") }
+}).count()
+
+
+/*In how many films is Roy L. McCardell credited as an actor? */
+db.movies.find({
+    cast: "Roy L. McCardell" 
+}).count()
+
+
+/*How many movies did Hal Roach directed? */
+db.movies.find({
+    directors: "Hal Roach"
+}).count()
+
+
+/*What is the movie with the earliest release year directed by Hal Roach?  */
+db.movies.find({
+     directors: { $in: ["Hal Roach"] }
+})
+
+
+
+/*  How many awards did Hal Roach’s movies win?  */
+
+db.movies.find({ directors: { $in: ["Hal Roach"] } }, 
+{title: 1, "awards.wins": 1, _id: 0 });
 
